06768920009c3c018a037eefeda1885ef10f315f
Nemanja <nemanjaapopovic@gmail.com>
2022-12-07 22:22:58 +0100

    Search fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,13 @@ import UserResults from "../components/layout/users/UserResults";
 import UserSearch from "../components/layout/users/UserSearch";
 
 const Home = () => {
-	return (
-		<>
-			<Alert />
-			<UserSearch />
-			<UserResults />
-		</>
-	);
+  return (
+    <>
+      <Alert />
+      <UserSearch />
+      <UserResults />
+    </>
+  );
 };
 
 export default Home;
